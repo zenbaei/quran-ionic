ff5362a0e9e0c2798e600a0c4b847826b260c6e4
islam zenbaei <zenbaei@gmail.com>
2018-09-04 17:38:36 +0300

    fix lib name

--- ios-run.sh
+++ ios-run.sh
@@ -5,5 +5,5 @@ if [ "$1" = "e" ]; then
 	ionic cordova emulate ios --target "iPhone-6s"
 elif [ "$1" = "d" ]; then
 	#-l hangs at splashscreen
-	ionic cordova run ios --devicee
+	ionic cordova run ios --device
 fi
